a897aba82a4ed241f1aee28f94c33b08ddd62d94
Rajan Saha Raju <imrajon0@gmail.com>
2018-09-16 22:43:03 +0600

    Create ClientTest.java

---
+++ Client/ClientTest.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ClientTest {
+
+    public static void main(String[] args) {
+
+        Client application = new Client();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.runClient();
+
+    }
+}
